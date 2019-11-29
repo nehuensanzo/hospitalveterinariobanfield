182e545dbc23d369c7d60b8a87cf8ec084406067
Nehuen Sanzo <nehuensanzo@gmail.com>
2019-11-29 00:23:18 -0300

    base exportada

--- productosdb.sql
+++ productosdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 01-11-2019 a las 14:59:07
--- Versión del servidor: 5.7.19
--- Versión de PHP: 5.6.31
+-- Servidor: 127.0.0.1:3308
+-- Tiempo de generación: 29-11-2019 a las 03:20:55
+-- Versión del servidor: 8.0.18
+-- Versión de PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `productos`
+-- Base de datos: `productosdb`
 --
+CREATE DATABASE IF NOT EXISTS `productosdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `productosdb`;
 
 -- --------------------------------------------------------
 
@@ -35,16 +37,16 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `precio` float NOT NULL,
   `categoria` text NOT NULL,
   PRIMARY KEY (`idProducto`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`nombre`, `precio`, `categoria`) VALUES
-('Royal Canin adultos raza grande', 2190, 'Alimentos'),
-('Royal Canin adultos raza  chica', 2300, 'Alimentos'),
-('Enrofloxacina', 200, 'Farmacia');
+INSERT INTO `productos` (`idProducto`, `nombre`, `precio`, `categoria`) VALUES
+(1, 'Royal Canin adultos raza grande', 2190, 'Alimentos'),
+(2, 'Royal Canin adultos raza  chica', 2300, 'Alimentos'),
+(3, 'Enrofloxacina', 200, 'Farmacia');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
